99ac008eac34e4ec75f2e2b32e40564b1fd5a13b
kuzan69 <113890965+kuzan69@users.noreply.github.com>
2023-05-02 11:34:36 +0200

    Add files via upload
    Hej

---
+++ WFA(ButtonÖvningar)/WFA(ButtonÖvningar)/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFA_ButtonÖvningar_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Green;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            lb1.Text = "Då";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txt1.Text += 1;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.Green;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
